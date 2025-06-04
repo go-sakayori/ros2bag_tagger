bc0c423c38036e780612a8729f08404e437d4089
Go Sakayori <go-sakayori@users.noreply.github.com>
2025-06-04 23:25:03 +0900

    Revert "feat: add json analysis (#4)"
    This reverts commit 0af1caa07ab9addd6027ffacbccd4a3544095062.

--- src/ros2bag_tagger/cli/__init__.py
+++ src/ros2bag_tagger/cli/__init__.py
@@ -1,6 +1,6 @@
 import typer
 
-from . import analysis, batch, convert, tagspec
+from . import batch, convert, tagspec
 
 app = typer.Typer(help="ros2bag tagging utility")
 
@@ -8,4 +8,3 @@ app = typer.Typer(help="ros2bag tagging utility")
 app.add_typer(convert.app, name="convert")
 app.add_typer(batch.app, name="batch")
 app.add_typer(tagspec.app, name="tagspec")
-app.add_typer(analysis.app, name="analysis")
